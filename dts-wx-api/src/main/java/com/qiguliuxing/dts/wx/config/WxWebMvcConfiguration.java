c5395deef161e1a4f2bbfc35d48b5806b9887f3d
liylong <1078509127@qq.com>
2024-07-15 11:32:03 +0800

    图片预览映射

--- dts-wx-api/src/main/java/com/qiguliuxing/dts/wx/config/WxWebMvcConfiguration.java
+++ dts-wx-api/src/main/java/com/qiguliuxing/dts/wx/config/WxWebMvcConfiguration.java
@@ -1,9 +1,11 @@
 package com.qiguliuxing.dts.wx.config;
 
+import java.io.File;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.qiguliuxing.dts.wx.annotation.support.LoginUserHandlerMethodArgumentResolver;
@@ -13,5 +15,13 @@ public class WxWebMvcConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
 		argumentResolvers.add(new LoginUserHandlerMethodArgumentResolver());
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		String rootPath = new File("").getAbsolutePath();
+		String pathString = rootPath.toString();
+		pathString = pathString.replace("\\", "/");
+		registry.addResourceHandler("/storage/**").addResourceLocations("file:"+pathString+"/dts/storage/");
 	}
 }
