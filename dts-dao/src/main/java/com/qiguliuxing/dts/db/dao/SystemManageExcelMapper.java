8ebd3e9a96875f369364bc81eff81e3548617b43
wuhong <2470395025@qq.com>
2024-08-23 14:31:05 +0800

    SystemManagerExcelMapper

---
+++ dts-dao/src/main/java/com/qiguliuxing/dts/db/dao/SystemManageExcelMapper.java
@@ -0,0 +1,8 @@
+package com.qiguliuxing.dts.db.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qiguliuxing.dts.db.domain.DtsReserve;
+
+public interface SystemManageExcelMapper extends BaseMapper<DtsReserve> {
+
+}
